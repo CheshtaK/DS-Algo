148601592fd08da8e0afe5d02fcb8db47df7a855
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-06-03 16:26:02 +0530

    deleting node at nth position

--- LLDeletion/main.cpp
+++ LLDeletion/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Node{
     int data;
     Node *next;
-} *start, *newptr, *save, *ptr;
+} *start, *newptr, *save, *ptr, *temp1, *temp2;
 
 void printList(Node *n){
     while(n != NULL){
@@ -31,14 +31,20 @@ void insertBeg(Node* np){
     }
 }
 
-void deleteNode(){
-    if(start == NULL)
-        cout<<"Overflow";
-    else{
-        ptr = start;
-        start = start -> next;
-        delete ptr;
+void deleteNode(int n){
+    temp1 = start;
+
+    if(n == 1){
+        start = temp1 -> next; //head now points to second node
+        delete temp1;
+        return;
     }
+    int i;
+    for(i = 0; i < n-2; i++)
+        temp1 = temp1 -> next; //n-1 node
+    temp2 = temp1 -> next; // n node
+    temp1 -> next = temp2 -> next; //n-1 points to n+1
+    delete temp2;
 }
 
 int main()
@@ -63,7 +69,10 @@ int main()
     printList(start);
     cout<<endl;
 
-    deleteNode();
+    int n;
+    cin>>n;
+
+    deleteNode(n);
 
     printList(start);
 
